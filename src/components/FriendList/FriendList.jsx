58ade224a606cc157896fd688e92d186a6b9b86b
havryliaksv <gavriliaks@gmail.com>
2024-03-28 15:05:06 +0200

    task 1-3 not css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,13 +3,11 @@ import FriendListItem from '../FriendListItem/FriendListItem.jsx';
 export default function FriendList({ friends }) {
   return (
     <ul>
-      {friends.map(friend => {
-        return (
-          <li key={friend.id}>
-            <FriendListItem friendItem={friend} />
-          </li>
-        );
-      })}
+      {friends.map(friend => (
+        <li key={friend.id}>
+          <FriendListItem friendItem={friend} />
+        </li>
+      ))}
     </ul>
   );
 }

affa23a3ce53f37b89c88cb0149a41f28562a0e8
havryliaksv <gavriliaks@gmail.com>
2024-03-30 11:57:10 +0200

    start css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import css from './Profile.module.css';
 export default function Profile({
   profile: {
     username,
@@ -8,27 +9,27 @@ export default function Profile({
   },
 }) {
   return (
-    <div>
-      <div>
+    <div className={css.container}>
+      <div className={css.profile}>
         <img src={avatar} alt="User avatar" />
-        <p>{username}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <p className={css.title}>{username}</p>
+        <p className={css.subtitle}>@{tag}</p>
+        <p className={css.subtitle}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={css.listStat}>
+        <li className={css.listStatItem}>
           <span>Followers</span>
           <span>{followers}</span>
         </li>
-        <li>
+        <li className={css.listStatItem}>
           <span>Views</span>
           <span>{views}</span>
         </li>
-        <li>
+        <l className={css.listStatItem}>
           <span>Likes</span>
           <span>{likes}</span>
-        </li>
+        </l>
       </ul>
     </div>
   );
